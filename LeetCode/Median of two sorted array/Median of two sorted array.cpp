8b662acfe7828302bab6fdcd61d2a7329c203ead
GarfieldPig <qi.yu@g42.ai>
2021-10-25 13:11:27 +0400

    Init commit

---
+++ LeetCode/Median of two sorted array/Median of two sorted array.cpp
@@ -0,0 +1,22 @@
+﻿// Median of two sorted array.cpp : Defines the entry point for the application.
+//
+
+#include "Median of two sorted array.h"
+
+using namespace std;
+
+int main()
+{
+	Solution s;
+	try
+	{
+		std::vector<int> nums1 = {1,3};
+		std::vector<int> nums2 = {2};
+		auto median = s.findMedianSortedArrays(nums1,nums2);
+		cout << median << endl;
+	}
+	catch (...) {
+		cout << "some error happen" << endl;
+	}
+	return 0;
+}
---
+++ LeetCode/Median of two sorted array/Median of two sorted array.h
@@ -0,0 +1,48 @@
+﻿// Median of two sorted array.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+// TODO: Reference additional headers your program requires here.
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(std::vector<int>& A, std::vector<int>& B) {
+        const size_t M = A.size();
+        const size_t N = B.size();
+        if (M > N) return findMedianSortedArrays(B, A);
+        double max_of_left = 0, min_of_right = 0;
+        size_t imin = 0, imax = M;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = (M + N + 1) / 2 - i;
+
+            if (i > 0 && A.at(i - 1) > B.at(j)) {
+                imax = i - 1;
+            }
+            else if (i < M && B.at(j - 1) > A.at(i)) {
+                imin = i + 1;
+            }
+            else {
+                if (i == 0) max_of_left = B.at(j - 1);
+                else if (j == 0) max_of_left = A.at(i - 1);
+                else max_of_left = max(A.at(i - 1), B.at(j - 1));
+
+                if ((M + N) % 2 == 1) {
+                    return max_of_left;
+                }
+
+                if (i == M) min_of_right = B.at(j);
+                else if (j == N) min_of_right = A.at(i);
+                else min_of_right = min(A.at(i), B.at(j));
+
+                return (max_of_left + min_of_right) / 2.0f;
+            }
+        }
+        return 0;
+    }
+};
