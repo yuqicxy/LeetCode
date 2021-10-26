5cb2852100b4ab7a763b1f857ce1525358ed8b5f
GarfieldPig <qi.yu@g42.ai>
2021-10-26 20:20:05 +0400

    Feat: 1.two-sum
    https://leetcode.com/problems/two-sum/

---
+++ 1.two-sum.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::vector<int> result;
+        std::unordered_multimap<int,int> hashmap;
+        for(unsigned int i =0; i < nums.size(); ++i)
+            hashmap.emplace(nums.at(i),i);
+        for(auto &&i : hashmap){
+            int val = target - i.first;
+            auto &&range = hashmap.equal_range(val);
+            for(auto findItr = range.first; findItr != range.second; ++findItr){
+                if(findItr->second != i.second){
+                    result.push_back(i.second);
+                    result.push_back(findItr->second);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
+// @lc code=end
+
