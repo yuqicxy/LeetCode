e8b282d91752d6bf68772772f65064788064a387
yuqicxy <yuqicxy@foxmail.com>
2021-10-28 14:38:16 +0400

    Feat: 62.unique-paths
    dynamic programming solution

--- 62.unique-paths.cpp
+++ 62.unique-paths.cpp
@@ -30,7 +30,21 @@ public:
      * @return int 
      */
     int uniquePaths(int m, int n) {
-        
+        if(m > n) return uniquePaths(n,m);
+        std::vector<int> dp(n * m, 0);
+        for(size_t i = 0; i < m; ++i){
+            for(size_t j = i; j < n; ++j){
+                if(i == 0 || j == 0){
+                    dp[i * n + j] = 1;
+                    continue;
+                }
+                if(j == i){
+                    dp[i * n + j - 1] = dp[(j-1) * n + i];
+                }
+                dp[i * n + j] = dp[i * n + j - 1] + dp[(i - 1) * n + j];
+            }
+        }
+        return dp[(m - 1) * n + n - 1];
     }
 };
 // @lc code=end
--- LeetCode/62.unique-paths/62.unique-paths.cpp
+++ LeetCode/62.unique-paths/62.unique-paths.cpp
@@ -3,10 +3,28 @@
 
 class Solution {
 public:
+    // int uniquePaths(int m, int n) {
+    //     if(m == 1 || n == 1)
+    //         return 1;
+    //     return uniquePaths(m,n-1) + uniquePaths(m-1,n);        
+    // }
+
     int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1)
-            return 1;
-        return uniquePaths(m,n-1) + uniquePaths(m-1,n);        
+        if(m > n) return uniquePaths(n,m);
+        std::vector<int> dp(n * m, 0);
+        for(size_t i = 0; i < m; ++i){
+            for(size_t j = i; j < n; ++j){
+                if(i == 0 || j == 0){
+                    dp[i * n + j] = 1;
+                    continue;
+                }
+                if(j == i){
+                    dp[i * n + j - 1] = dp[(j-1) * n + i];
+                }
+                dp[i * n + j] = dp[i * n + j - 1] + dp[(i - 1) * n + j];
+            }
+        }
+        return dp[(m - 1) * n + n - 1];
     }
 };
 
