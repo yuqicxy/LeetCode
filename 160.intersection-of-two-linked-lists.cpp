5d8f257b5b8ca2d58a147cd9ecabcf3af6e30e2e
yuqicxy <yuqicxy@foxmail.com>
2021-10-29 16:58:19 +0400

    Feat: 160.intersection-of-two-linked-lists
    https://leetcode.com/problems/intersection-of-two-linked-lists/

---
+++ 160.intersection-of-two-linked-lists.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=160 lang=cpp
+ *
+ * [160] Intersection of Two Linked Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *A = headA;
+        ListNode *B = headB;
+        while(A != B)
+        {
+            A = A != nullptr ? A->next : headB;
+            B = B != nullptr ? B->next : headA;
+        }
+        return A;
+    }
+};
+// @lc code=end
+
