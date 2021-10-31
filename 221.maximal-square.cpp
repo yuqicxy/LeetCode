58389504abb793d8eabf4773ac8776f78a3febe4
yuqicxy <yuqicxy@foxmail.com>
2021-10-31 17:50:37 +0400

    Feat: 221.maximal square
    Brutal force solution
    
    https://leetcode.com/problems/maximal-square/

---
+++ 221.maximal-square.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=221 lang=cpp
+ *
+ * [221] Maximal Square
+ */
+
+// @lc code=start
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int max = 0;
+        unsigned height = matrix.size();
+        unsigned width = matrix[0].size();
+        vector<vector<bool>> degenrateMatrix(height,std::vector<bool>(width));
+        for(size_t i = 0; i < height; ++i){
+            for(size_t j = 0; j < width; ++j){
+                if(matrix[i][j] != '0'){
+                    degenrateMatrix[i][j] = 1;
+                    max = 1;
+                }
+                else
+                    degenrateMatrix[i][j] = 0;
+            }
+        }
+        recrusive(degenrateMatrix,max,2);
+        return max;
+    }
+
+    void recrusive(vector<vector<bool>> &matrix,int &max,int depth){
+        unsigned height = matrix.size();
+        unsigned width = matrix[0].size();
+        if(width < 2 || height < 2) return;
+        bool validFlag = false;
+        vector<vector<bool>> degenrateMatrix(height - 1,std::vector<bool>(width - 1));
+        for(size_t i = 0; i < height - 1; ++i){
+            for(size_t j = 0; j < width - 1; ++j){
+                if(matrix[i][j]   &&
+                matrix[i][j + 1]  &&
+                matrix[i + 1][j]  &&
+                matrix[i + 1][j + 1]){
+                    degenrateMatrix[i][j] = true;
+                    max = pow(depth,2);
+                    if(!validFlag)
+                        validFlag = true;
+                }
+                else
+                    degenrateMatrix[i][j] = false;
+            }
+        }
+        if(!validFlag) return;
+        recrusive(degenrateMatrix,max,depth + 1);
+        return;
+    }
+};
+// @lc code=end
