dd76209c1146697ccb611bae71b36fbe878ef774
yuqicxy <yuqicxy@foxmail.com>
2021-10-31 14:45:15 +0400

    Feat: 20.valid-parentheses
    https://leetcode.com/problems/valid-parentheses/

---
+++ 20.valid-parentheses.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> charStack; 
+        for(unsigned i = 0; i < s.length(); ++i){
+            const char c = s.at(i);
+            if(c == '(' || c == '[' || c == '{'){
+                charStack.push(c);
+                continue;
+            }
+            else if(!charStack.empty()){
+                const char s = charStack.top();
+                if((s == '(' && c == ')') || (s == '[' && c == ']') || (s == '{' && c == '}'))
+                {
+                    charStack.pop();
+                    continue;
+                }   
+            }
+            return false;
+        }
+        if(charStack.empty())
+            return true;
+        else
+            return false;
+    }
+};
+// @lc code=end
+
