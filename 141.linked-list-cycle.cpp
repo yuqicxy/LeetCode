ac33a1ee295061190cc7b9141471ae3904eb1875
yuqicxy <yuqicxy@foxmail.com>
2021-11-03 22:02:15 +0400

    Feat: 141.linked-list-cycle
    https://leetcode.com/problems/linked-list-cycle/

---
+++ 141.linked-list-cycle.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=141 lang=cpp
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /* my solution*/
+    // bool hasCycle(ListNode *head) {
+    //     if(head == nullptr || head->next == nullptr) return false;
+    //     ListNode *singleStep = head;
+    //     ListNode *doubleStep = head->next->next;
+    //     while(singleStep != doubleStep){
+    //         if(singleStep == nullptr 
+    //             || doubleStep == nullptr
+    //             || doubleStep->next == nullptr)  
+    //             return false;
+    //         singleStep = singleStep->next;
+    //         doubleStep = doubleStep->next->next;
+    //     }
+    //     return true;
+    // }
+    /**
+     * @brief other guys' solution
+     * 
+     * @param head 
+     * @return true 
+     * @return false 
+     */
+    bool hasCycle(ListNode *head) {
+        ListNode *singleStep = head;
+        ListNode *doubleStep = head;
+        while(doubleStep != nullptr && doubleStep->next != nullptr){
+            singleStep = singleStep->next;
+            doubleStep = doubleStep->next->next;
+            if(singleStep == doubleStep) return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
