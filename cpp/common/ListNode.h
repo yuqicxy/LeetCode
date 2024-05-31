efda1d6b1b12da602361c1526436401bca88405a
yuqicxy <yuqicxy@foxmail.com>
2024-05-31 10:50:46 +0400

    add initializater constructor and format friend function

--- cpp/common/ListNode.h
+++ cpp/common/ListNode.h
@@ -1,5 +1,34 @@
+#include <initializer_list>
+#include <iostream>
+
 struct ListNode {
     int val;
     ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+   
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+   
     ListNode(int x) : val(x), next(nullptr) {}
+   
+    ListNode(const std::initializer_list<int> &list){
+        val = *list.begin();
+        auto pre = this;
+        for(auto itr = list.begin()+1; itr < list.end(); ++itr){
+            auto node = new ListNode(*itr);
+            pre->next = node;
+            pre = node;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const ListNode* node){
+        os<<"[";
+        while (node != nullptr)
+        {
+            os << node->val << ",";
+            node = node->next;
+        }
+        os<<'\b'<<"]"<<std::endl;
+        return os;
+    }
 };
