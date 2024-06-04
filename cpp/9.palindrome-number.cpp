f029c569933f16e15dbb4eb943b62b1c63d79462
yuqicxy <yuqicxy@foxmail.com>
2024-06-04 22:53:55 +0400

    submit 9.palindrome

---
+++ cpp/9.palindrome-number.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+#include<vector>
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+
+        int tmp = x;
+        unsigned int reversed = 0;
+        while(tmp != 0){
+            int digit = tmp % 10;
+            reversed = reversed * 10 + digit;
+            tmp = tmp / 10;
+        }
+
+        if(reversed == x)
+            return true;
+        
+        return false;
+    }
+};
+// @lc code=end
+
+#include<iostream>
+int main(){
+    Solution s;
+    std::cout << std::boolalpha;
+    std::cout << s.isPalindrome(121) << std::endl;
+    return 0;
+}
