9a244ed23caaa8cfa25dab7493513478f1e583af
yuqicxy <yuqicxy@foxmail.com>
2024-09-01 10:21:44 +0400

    Fix: 841. add depth first search solution

--- cpp/841.keys-and-rooms.cpp
+++ cpp/841.keys-and-rooms.cpp
@@ -7,8 +7,7 @@
 #include <vector>
 #include <deque>
 using namespace std;
-// @lc code=start
-class Solution {
+class Solution_Qi {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         set<int> results;
@@ -29,10 +28,32 @@ public:
         return false;
     }
 };
+
+// @lc code=start
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> openedRoom(rooms.size(),false);
+        openedRoom[0] = true;
+        int count = dfs(rooms,0,openedRoom) + 1;
+        return count == rooms.size();
+    }
+
+    int dfs(vector<vector<int>>& rooms, const int &key,vector<bool>& openedRoom){
+        int count = 0;
+        for(auto subKey : rooms[key]){
+            if(openedRoom[subKey]) continue;
+            openedRoom[subKey] = true;
+            count += dfs(rooms,subKey,openedRoom) + 1;
+        }
+        return count;
+    }
+};
 // @lc code=end
 int main(){
     Solution s;
-    vector<vector<int>> rooms{{1,3},{3,0,1},{2},{0}};
+    // vector<vector<int>> rooms{{1,3},{3,0,1},{2},{0}};
+    vector<vector<int>> rooms{{1},{2},{3},{}};
     bool result = s.canVisitAllRooms(rooms);
     return result;
 }
