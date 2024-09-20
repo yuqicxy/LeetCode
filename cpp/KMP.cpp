6a573d7f5034c6e2daa1b5a41ed34b9529801b09
yuqicxy <yuqicxy@foxmail.com>
2024-09-20 14:15:51 +0400

    Feat: Knuth-Morris-Pratt KMP String Matching Algorithm

--- cpp/KMP.cpp
+++ cpp/KMP.cpp
@@ -17,11 +17,22 @@ using namespace std;
 /**
  * @brief Knuth Morris Pnatt algorithm (substring matching)
  * 
+ *  https://www.youtube.com/watch?v=V5-7GzOfADQ
+ * 
  */
 class KMP{
 public:
     int findPattern(const string& input, const string& pattern){
-
+        vector<int> pi = calKMPPrefixArray(pattern);
+        int j = 0;
+        for(size_t i = 0; i < input.size(); ++i){
+            bool flag  = true;
+            while(!(j < 0) && input[i] != pattern[j])
+                j = pi[j] - 1;
+            j++;
+            if(!(j < pi.size())) return (int)(i - pattern.size()) + 1;
+        }
+        return -1;
     }
 
     /**
@@ -39,32 +50,36 @@ public:
      * @return std::vector<size_t>  prefix value, vector length is equal to string size of pattern 
      */
     vector<int> calKMPPrefixArray(const string& pattern){
-        vector<int> ans(pattern.size(), 0);
-        for(int i = 0; i < pattern.size(); ++i){
-            for(int k = 0; k < i; ++k){
-                if(pattern.substr(0, k) == pattern.substr(i - k + 1, k))
-                    ans[i] = k;
-            }
+        vector<int> pi(pattern.size(), 0);
+        for(int i = 1; i < pattern.size(); ++i){
+            int j = pi[i - 1];
+            while(j > 0 && pattern[i] != pattern[j])
+                j = pi[j - 1];
+            if(pattern[i] == pattern[j])
+                j++;
+            pi[i] = j;
         }
-        return ans;
+        return pi;
     }
 };
 
 int main(){
+    KMP solution;
+
     string test1 = "abcdefgh";
     string pattern1 = "def";
-
+    cout << solution.findPattern(test1,pattern1) << endl; /** pos 3 */
 
     string test2 = "abcdabcabcdf";
     string pattern2 = "abcdf";
+    cout << solution.findPattern(test2,pattern2) << endl; /** pos 7 */
 
+    string test3 = "abcdabcabcdf";
+    string pattern3 = "dd";
+    cout << solution.findPattern(test3,pattern3) << endl; /** pos -1 */
 
-    string prefixTest = "abcabcd";
-    KMP solution;
+    string prefixTest = "ababcababa";
    
     for (auto &&i : solution.calKMPPrefixArray(prefixTest))
-    {
         cout<<','<<i;
-    }
-    
 }
