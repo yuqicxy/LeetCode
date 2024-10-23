6a647779a41e2ad0696efd80ad03bd2b41cc8791
yuqicxy <yuqicxy@foxmail.com>
2024-10-23 18:10:18 +0400

    Optimized: [2641] Cousins in Binary Tree II

--- cpp/2641.cousins-in-binary-tree-ii.cpp
+++ cpp/2641.cousins-in-binary-tree-ii.cpp
@@ -23,49 +23,38 @@ using namespace std;
 class Solution {
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
+        root->val = 0;
         queue<TreeNode*> queue;
         queue.push(root);
-        vector<long long> depthSum;
         while(!queue.empty()){
             int sz = queue.size();
             long long sum = 0;
+            vector<TreeNode*> subNodes;
             while(sz--){
                 TreeNode* nd = queue.front();
                 queue.pop();
-                sum += nd->val;
-                if(nd->left != nullptr)
+                if(nd->left != nullptr){
                     queue.push(nd->left);
-                if(nd->right != nullptr)
+                    sum += nd->left->val;
+                }
+                if(nd->right != nullptr){
                     queue.push(nd->right);
+                    sum += nd->right->val;
+                }
+                subNodes.push_back(nd);
             }
-            depthSum.push_back(sum);
-        }
-        root->val = 0;
-        queue.push(root);
-        int level = 1;
-        while(level < depthSum.size()){
-            int sz = queue.size();
-            long long sum = depthSum.at(level++);
-            while(sz--){
-                TreeNode* nd = queue.front();
-                queue.pop();
-                int tmp = sum;
+
+            for_each(subNodes.begin(), subNodes.end(), [sum](TreeNode* nd){
+                long long tmp = sum;
                 if(nd->left != nullptr)
                     tmp -= nd->left->val;
                 if(nd->right != nullptr)
                     tmp -= nd->right->val;
-                
                 if(nd->left != nullptr)
-                {
                     nd->left->val = tmp;
-                    queue.push(nd->left);
-                }
                 if(nd->right != nullptr)
-                {
                     nd->right->val = tmp;
-                    queue.push(nd->right);
-                }
-            }
+            });
         }
         return root;
     }
