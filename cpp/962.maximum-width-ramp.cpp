520df2e1cc78683916cc16d3158395db5d2b2ffe
yuqicxy <yuqicxy@foxmail.com>
2024-11-02 11:19:49 +0400

    Fix: Update Solution

--- cpp/962.maximum-width-ramp.cpp
+++ cpp/962.maximum-width-ramp.cpp
@@ -42,11 +42,15 @@ public:
         for(int i = 0; i < nums.size(); ++i){
             indexes.push_back(i);
         }
-        sort(indexes.begin(),indexes.end(), [&nums](int left, int right){
-            return nums[left] < nums[right];
+        sort(indexes.begin(),indexes.end(), [&](int left, int right){
+            if(nums[left] < nums[right]) return true;
+            //Add additional rule for the duplicate number, make sure the lower index appear firstly.
+            else if(nums[left] == nums[right]) return left < right; 
+            return false;
         });
 
-        int ans = 0, mn=INT_MAX;
+        int ans = 0;
+        int mn = nums.size();
         for(int i = 0; i < nums.size(); ++i){
             ans = max(ans, indexes[i] - mn);
             mn = min(mn, indexes[i]);
@@ -59,8 +63,20 @@ public:
 
 int main(){
     Solution s;
-    auto nums = vector<int>{6,0,8,2,1,5};
-    cout << s.maxWidthRamp(nums) << endl;
-    nums = vector<int>{9,8,1,0,1,9,4,0,4,1};
-    cout << s.maxWidthRamp(nums) << endl;
+    // auto nums = vector<int>{6,0,8,2,2,1,5,1};
+    // cout << s.maxWidthRamp(nums) << endl;
+    // nums = vector<int>{9,8,1,0,1,9,4,0,4,1};
+    // cout << s.maxWidthRamp(nums) << endl;
+    
+    auto nums = vector<int>{
+        3 ,28,15,1 ,4 ,
+        12,6 ,19,8 ,15,
+        3 ,9 ,6 ,4 ,13,
+        12,6 ,12,10,1 ,
+        2 ,1 ,4 ,1 ,4 ,
+        0 ,0 ,1 ,1 ,0 };
+
+    nums = vector<int>{3,28,15,1,4,12,6,19,8,15,3,9,6,4,13,12,6,12,10,1,2,1,4,1,4,0,0,1,1,0};
+
+    cout << s.maxWidthRamp(nums) << endl; //25
 }
