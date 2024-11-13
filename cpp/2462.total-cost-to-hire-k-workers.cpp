1407e16ff85580dadd8c16a94250df8205141c41
yuqicxy <yuqicxy@foxmail.com>
2024-11-13 15:09:27 +0400

    Fix: [2462] Total Cost to Hire K Workers
    Improve the solution by using two priority queue instead of one containing tuple which save index number

--- cpp/2462.total-cost-to-hire-k-workers.cpp
+++ cpp/2462.total-cost-to-hire-k-workers.cpp
@@ -14,33 +14,29 @@ class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
         long long ans = 0;
-        priority_queue<tuple<int,int>, vector<tuple<int,int>>, greater<tuple<int,int>>> pq;
-        if(costs.size() > 2 * candidates){
-            for(int i = 0; i < candidates; ++i){
-                pq.emplace(costs[i], i);
-                pq.emplace(costs[costs.size() - 1 - i], costs.size() - 1 - i);
-            }
-        }else{
-            for(int i = 0; i < costs.size(); ++i)
-                pq.emplace(costs[i], i);
-        }
-            
-        int i = candidates, j = costs.size() - candidates - 1;
-        while(k--){
-            auto [cost, id] = pq.top();
-            pq.pop();
-            ans += cost;
-            if(i <= j){
-                if(id < i){
-                    pq.emplace(costs[i], i);
-                    ++i;
-                }else if(id > j){
-                    pq.emplace(costs[j], j);
-                    --j;
-                }
-            }
-        }
+        priority_queue<int, vector<int>, greater<int>> left;
+        priority_queue<int, vector<int>, greater<int>> right;
 
+        int i = 0;
+        int j = costs.size() - 1;
+        while(k--){
+            while(left.size() < candidates && i <= j)
+                left.push(costs[i++]);
+
+            while(right.size() < candidates && i <= j)
+                right.push(costs[j--]);
+            
+            int leftMin = left.empty() ? INT_MAX : left.top();
+            int rightMin = right.empty() ? INT_MAX : right.top();
+            if(leftMin <= rightMin){
+                ans += leftMin;
+                left.pop();
+            }
+            else{
+                ans += rightMin;
+                right.pop();
+            }
+        }
         return ans;
     }
 };
@@ -56,5 +52,11 @@ int main(){
     k = 3;
     candidates = 3;
     cout << s.totalCost(costs, k, candidates) <<endl;
+
+    costs = {4866,4857,4378,4876,3594,4874,4717,4680,4813,4938,4156,4724};
+    k = 9;
+    candidates = 2;
+    cout << s.totalCost(costs, k, candidates) <<endl; //40785
+
     return 0;
 }
